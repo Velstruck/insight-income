cd75e7a8b75440b87c115b850259df152808aba4
Vasudev <79050603+Velstruck@users.noreply.github.com>
2025-03-22 16:09:27 +0530

    signup/login form FE

--- frontend/expense-tracker/src/components/Inputs/Input.jsx
+++ frontend/expense-tracker/src/components/Inputs/Input.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa6'
 const Input = ({value, onChange, placeholder,label,type}) => {
     const [showPassword, setShowPassword] = useState(false)
 
@@ -9,9 +9,32 @@ const Input = ({value, onChange, placeholder,label,type}) => {
 
   return (
     <div>
-        <label className=''>{label}</label>
-        <div>
-            
+        <label className='text-[13px] text-slate-800'>{label}</label>
+        <div className='input-box'>
+            <input
+            type={type === 'password' ? showPassword ? 'text' : 'password' : type}
+            placeholder={placeholder}
+            className='w-full bg-transparent outline-none'
+            value={value}
+            onChange={(e)=>onChange(e)}
+            />
+            {type == 'password' && (
+            <>
+              {showPassword ? (
+                <FaRegEye
+                size={22}
+                className='text-primary cursor-pointer'
+                onClick={()=>toggleShowPassword()}
+                />
+              ):(
+                <FaRegEyeSlash
+                size={22}
+                className='text-slate-400 cursor-pointer'
+                onClick={()=>toggleShowPassword()}
+                />
+              )}
+            </>  
+            )}
         </div>
     </div>
   )
