225e4ea71e1e372b096d64c70edff5611caa3838
Vasudev <79050603+Velstruck@users.noreply.github.com>
2025-04-22 17:22:39 +0530

    backend restore

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
@@ -7,9 +8,6 @@ const incomeRoutes = require("./routes/incomeRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
 
-const dotenv = require("dotenv");
-dotenv.config();
-
 const app = express();
 
 //CORS ke liye middlewares
@@ -17,6 +15,8 @@ const app = express();
 app.use(
     cors({
         origin: process.env.CLIENT_URL,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
     })
 )
@@ -35,8 +35,6 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
-
-module.exports = app;
-
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 
